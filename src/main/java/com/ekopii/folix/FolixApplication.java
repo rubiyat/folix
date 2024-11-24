d5d2b37cf1f3daf76e9da1e12aaa45980bf7ed71
rubiyat <rubiyat21@gmail.com>
2024-11-24 20:41:39 +0600

    first commit

---
+++ src/main/java/com/ekopii/folix/FolixApplication.java
@@ -0,0 +1,13 @@
+package com.ekopii.folix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ekopii/folix/FolixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekopii.folix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
